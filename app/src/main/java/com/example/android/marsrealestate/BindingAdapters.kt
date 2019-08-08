904a23eddf694b638e84aa9d0bfc8de87f3baa77
ananthrajsingh <ananthraj.singh@gmail.com>
2019-08-09 00:41:09 +0530

    Add glide to load image, Add binding

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -17,6 +17,22 @@
 
 package com.example.android.marsrealestate
 
-// TODO (06) Create the Binding Adapter, converting the imgUrl to a URI with the https scheme
-// TODO (07) Use Glide to load the imgUri into the imgView
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+@BindingAdapter("imageUrl")
+fun bindImage(imageView: ImageView, imageUrl: String?) {
+    imageUrl?.let {
+        val imageUri = imageUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imageView.context)
+                .load(imageUri)
+                .apply(RequestOptions()
+                        .placeholder(R.drawable.loading_animation)
+                        .error(R.drawable.ic_broken_image))
+                .into(imageView)
+    }
+}
 // TODO (11) Add the requestOptions for the placeholder and error into the Glide call
